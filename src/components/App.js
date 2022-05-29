ad8570eaa34d94984ee6e1e9219b0ca82b2cb8a5
FabioDiCeglie <dcfabio96@gmail.com>
2022-05-29 18:50:10 +0200

    creating Links component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,10 @@
-function App() {
-  return <div></div>;
+import React, { Component } from "react";
+import LinkList from "./LinkLiks";
+
+class App extends Component() {
+  render() {
+    return <LinkList />;
+  }
 }
 
 export default App;
